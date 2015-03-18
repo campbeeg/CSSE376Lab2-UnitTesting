2479c9eeed2fd835ae8b1185b635c47c8b028d60
Ethan Campbell <campbeeg@rose-hulman.edu>
2015-03-18 12:34:37 -0400

    All tests pass
    Ethan Campbell passed all unit tests

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -7,6 +7,58 @@ namespace ExpediaTest
 	[TestFixture()]
 	public class FlightTest
 	{
-		//TODO Task 7 items go here
+        private readonly DateTime dateThatFlightLeaves = new DateTime(2015, 3, 18);
+		private readonly DateTime dateThatFlightReturns = new DateTime(2015, 3, 19);
+
+        private readonly int miles = 100;
+
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            var target = new Flight(dateThatFlightLeaves, dateThatFlightReturns, miles);
+            Assert.IsNotNull(target);
+        }
+
+        [Test()]
+        public void TestThatFlightHasCorrectMilesForAOneHundredMileFlight()
+        {
+            var target = new Flight(dateThatFlightLeaves, dateThatFlightReturns, miles);
+            Assert.AreEqual(100, target.Miles);
+        }
+
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForAOneDayFlight()
+        {
+            var target = new Flight(dateThatFlightLeaves, dateThatFlightReturns, miles);
+            Assert.AreEqual(220, target.getBasePrice());
+        }
+
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForATwoDayFlight()
+        {
+            var target = new Flight(dateThatFlightLeaves, new DateTime(2015, 3, 20), miles);
+            Assert.AreEqual(240, target.getBasePrice());
+        }
+
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForAThreeDayFlight()
+        {
+            var target = new Flight(dateThatFlightLeaves, new DateTime(2015, 3, 21), miles);
+            Assert.AreEqual(260, target.getBasePrice());
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatFlightThrowsOnBadLength()
+        {
+            new Flight(dateThatFlightLeaves, dateThatFlightReturns, -5);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatFlightThrowsOnBadDates()
+        {
+            new Flight(dateThatFlightLeaves, new DateTime(2015, 3, 1), miles);
+        }
 	}
 }
